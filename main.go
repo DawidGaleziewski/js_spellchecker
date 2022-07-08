df5b58f6f347ae43bb329701cd5840f76b207940
Dawid <dawidgalsino@vp.pl>
2022-07-08 23:56:35 +0200

    feat(setup): setup basic terminal input

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+
+	if len(os.Args) == 1 {
+		log.Println("No argument provided")
+		return
+	}
+
+	inputBlob := os.Args[1]
+
+	fmt.Println(inputBlob)
+}
+
+//getVariableNames accepts string and returns variable declarations in javascript if there are any
+func GetVariableNames(textBlob string) []string {
+	r := regexp.MustCompile("(const) ([^ \n]*)")
+	captureGroups := r.FindAllStringSubmatch(textBlob, -1)
+	var variableNames []string
+
+	if captureGroups == nil {
+		fmt.Println("no matches found")
+		return variableNames
+	}
+
+	for _, group := range captureGroups {
+		variableNames = append(variableNames, group[2])
+	}
+
+	return variableNames
+}
