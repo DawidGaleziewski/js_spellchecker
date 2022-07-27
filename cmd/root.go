0e4b1482cf335efec51e4bc89ee864e252122f2e
uniters_ngo <uniters-fundation@tutanota.com>
2022-07-27 23:11:08 +0200

    wip: transfer to cobra

--- cmd/root.go
+++ cmd/root.go
@@ -36,6 +36,10 @@ func Execute() {
 	}
 }
 
+func addSubcommandsPallets(){
+	rootCmd.AddCommand(check.CheckCmd)
+}
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,6 +50,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addSubcommandsPallets()
 }
 
 
